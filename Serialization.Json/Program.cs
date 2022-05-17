94be78d0cc9c21f727afefa8d575a4d643d72dee
DuanDang <duandang1403@gmail.com>
2022-05-17 11:11:46 +0700

    Add program (byte,xml,json)

--- Serialization.Json/Program.cs
+++ Serialization.Json/Program.cs
@@ -1,14 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Serialization
+namespace Serialization.Json
 {
-    [Serializable]
     public class Student
     {
         public int Id { get; set; } = 1;
@@ -22,14 +21,13 @@ namespace Serialization
         {
             var student = new Student
             {
-                Id = 4,
+                Id = 1,
                 FirstName = "Nguyen Van",
-                LastName = "C",
+                LastName = "A",
                 DateOfBirth = new DateTime(1990, 12, 30)
             };
             Console.WriteLine("Original object:");
             Print(student);
-           
             Save(student);
             var nva = Load();
             Console.WriteLine("Deserialized object:");
@@ -40,21 +38,24 @@ namespace Serialization
         {
             Console.WriteLine($"Id: {student.Id}\r\nFirst Name: {student.FirstName}\r\nLast Name: {student.LastName}\r\nDate of birth: {student.DateOfBirth.ToShortDateString()}");
         }
+
         static void Save(Student student)
         {
-            using (var stream = File.OpenWrite("data.bin"))
+            using (var stream = File.OpenWrite("data.xml"))
             {
-                var formatter = new BinaryFormatter();
-                formatter.Serialize(stream, student);
+                var writer = new StreamWriter(stream) { AutoFlush = true };
+                var serializer = new JsonSerializer();
+                serializer.Serialize(writer, student);
             }
         }
         static Student Load()
         {
             Student student;
-            using (var stream = File.OpenRead("data.bin"))
+            using (var stream = File.OpenRead("data.xml"))
             {
-                var formatter = new BinaryFormatter();
-                student = formatter.Deserialize(stream) as Student;
+                var reader = new StreamReader(stream);
+                var serializer = new JsonSerializer();
+                student = serializer.Deserialize(reader, typeof(Student)) as Student;
             }
             return student;
         }
---
+++ Serialization.XML/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serialization.XML
+{
+        public class Student
+        {
+            public int Id { get; set; } = 1;
+            public string FirstName { get; set; } = "";
+            public string LastName { get; set; } = "";
+            public DateTime DateOfBirth { get; set; } = DateTime.Now;
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                var student = new Student
+                {
+                    Id = 1,
+                    FirstName = "Nguyen Van",
+                    LastName = "A",
+                    DateOfBirth = new DateTime(1990, 12, 30)
+                };
+                Console.WriteLine("Original object:");
+                Print(student);
+                Save(student);
+                var nva = Load();
+                Console.WriteLine("Deserialized object:");
+                Print(nva);
+                Console.ReadKey();
+            }
+            static void Print(Student student)
+            {
+                Console.WriteLine($"Id: {student.Id}\r\nFirst Name: {student.FirstName}\r\nLast Name: {student.LastName}\r\nDate of birth: {student.DateOfBirth.ToShortDateString()}");
+            }
+            static void Save(Student student)
+            {
+                using (var stream = File.OpenWrite("data.xml"))
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(Student));
+                    serializer.Serialize(stream, student);
+            }
+            }
+            static Student Load()
+            {
+                Student student;
+                using (var stream = File.OpenRead("data.xml"))
+                {
+                    var serializer = new XmlSerializer(typeof(Student));
+                    student = serializer.Deserialize(stream) as Student;
+                }
+                return student;
+            }
+        }
+    
+}
